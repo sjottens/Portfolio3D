bf60b21cf9cced8680316fc98f3fe356890e272c
Rogier Ottens <rvottens@gmail.com>
2025-12-02 21:35:51 +0100

    text correction

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -34,7 +34,7 @@ const Hero = () => {
             <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
                 <div className="flex flex-col gap-7">
                     <div className="hero-text">
-                        <h1>Shapping
+                        <h1>Shaping
                             <span className="slide">
                                 <span className="wrapper">
                                     {words.map((word) => (
