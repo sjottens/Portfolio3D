75a8261c66bf3467bf339ad801f4a4aa61c81671
Rogier Ottens <rvottens@gmail.com>
2026-01-05 14:20:49 +0100

    added google tag manager

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import ReactGA from "react-ga4";
+import TagManager from 'react-gtm-module';
 
 ReactGA.initialize("G-T4QNDV0H95");
 
@@ -12,6 +13,11 @@ ReactGA.send({
   page: window.location.pathname, 
   title: "Portfolio3D" 
 });
+
+// Initialize GTM with GTM ID
+const tagManagerArgs = {
+  gtmId: 'GTM-WRSML5N2',
+};
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
